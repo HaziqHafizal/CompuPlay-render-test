b6aebd7a7251709001323da7834bf60d8fd26636
Muhammad Haziq <152416635+HaziqHafizal@users.noreply.github.com>
2026-01-13 02:02:31 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,9 @@ class AppServiceProvider extends ServiceProvider
     {
         // Register notification observer
         Notification::observe(NotificationObserver::class);
+
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
